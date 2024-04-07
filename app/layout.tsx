67b6742dd8822e59b7718813b8c8ca7dc6b8f041
droobun <avwilliams1995@gmail.com>
2024-04-07 11:01:53 -0400

    header bcomponent created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolte top-[-6rem] -z-10 right[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolte top-[-1rem] -z-10 left[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]"></div>
-        {children}
+        <Header/>
       </body>
     </html>
   );
