8f7cb8a93aa29125bf78020d2709bedf5ab01c32
droobun <avwilliams1995@gmail.com>
2024-04-11 00:28:30 -0400

    heder motion

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full "
-        initial={{ y: -150, x: "-50%", opacity: 0 }}
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         transition={{
           type: "spring",
@@ -29,7 +29,7 @@ export default function Header() {
             <motion.li
               key={link.hash}
               className="h-3/4 flex items-center justify-center relative"
-              initial={{ y: -150, opacity: 0 }}
+              initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{
                 type: "spring",
